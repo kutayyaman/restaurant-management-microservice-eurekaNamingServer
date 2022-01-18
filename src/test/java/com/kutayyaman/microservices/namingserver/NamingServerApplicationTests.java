895ca50869cd4ef6d1486afcf9b0129fe127517d
Kutay Yaman <yamankutay1@gmail.com>
2022-01-18 23:56:11 +0300

    Initial commit

---
+++ src/test/java/com/kutayyaman/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
